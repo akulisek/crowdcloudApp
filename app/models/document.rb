1992086851c61dd7f92e94cfa9acea606e4539d4
adam <akulisek@gmail.com>
2014-07-29 09:48:34 +0200

    Modified for elasticsearch

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ActiveRecord::Base
-    has_many :comments
+  has_many :comments
+  has_many :attachments
+
+  def to_indexable
+    {
+        content: attachments.map {|a| a.pages.map(&:text)}.flatten.join(' ')
+    }
+  end
 end
